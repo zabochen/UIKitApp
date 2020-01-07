6e7b0f5ab9f56858b2937987c23f0dabb1826b82
Taras <zabochen@gmail.com>
2020-01-07 22:06:33 +0200

    Change viewController

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -2,19 +2,50 @@
 //  ViewController.swift
 //  UIKitApp
 //
-//  Created by Taras on 07.01.2020.
-//  Copyright © 2020 Taras. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Hide label
+        hideLabel()
+        
+        // Change button
+        self.button.setTitle("Show some text in lable", for: UIControl.State.normal)
+        self.button.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        self.button.backgroundColor = UIColor.black
     }
 
-
+    @IBAction func clickButton(_ button: UIButton) {
+        // Change label
+        self.label.text = "Hello, label!"
+        self.label.font = label.font.withSize(35)
+        self.label.textColor = UIColor.red
+        
+        // Sender Tag
+        // All views has "default tag": 0
+        // We can add tag in Interface Bulder
+        let uiViewTag: Int = button.tag
+        if uiViewTag == 100 {
+            print(uiViewTag)
+        }
+        
+        // Show label
+        showLabel()
+    }
+    
+    private func showLabel() {
+        self.label.isHidden = false
+    }
+    
+    private func hideLabel() {
+        self.label.isHidden = true
+    }
 }
 
