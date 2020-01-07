4fe92e89197c16c040b77748e8f4f8d3fa51b7ee
Taras <zabochen@gmail.com>
2020-01-07 20:11:31 +0200

    Initial Commit

---
+++ UIKitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitApp
+//
+//  Created by Taras on 07.01.2020.
+//  Copyright © 2020 Taras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
