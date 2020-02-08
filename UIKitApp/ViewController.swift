eeee60c49c5f075cabc6d16e8eac73c75651ec59
Taras <Taras@MacBook.local>
2020-02-08 19:47:17 +0200

    UI Slider

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Init Label
+        initLabel()
+        
+        // Init Slider
+        initSlider()
     }
 
-
+    @IBAction func actionSlider(_ slider: UISlider){
+        label.text = String(slider.value)
+        let backgroundColor  = self.view.backgroundColor
+        self.view.backgroundColor = backgroundColor?.withAlphaComponent(CGFloat(slider.value))
+    }
+    
+    private func initLabel(){
+        label.text = String(slider.value)
+        label.textAlignment = NSTextAlignment.center
+        label.numberOfLines = 2
+    }
+    
+    private func initSlider(){
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        
+        slider.minimumTrackTintColor = .red
+        slider.maximumTrackTintColor = .green
+        slider.thumbTintColor = .black
+    }
 }
-
