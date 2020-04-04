76771ada94190a6787df0d89727aca255b6aa6b3
zabochen <zabochen@gmail.com>
2020-04-04 15:10:45 +0300

    View Array

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -9,6 +9,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var button: UIButton!
+    @IBOutlet var buttonScope: [UIButton]!
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,9 +21,16 @@ class ViewController: UIViewController {
         self.button.setTitle("Show some text in lable", for: UIControl.State.normal)
         self.button.setTitleColor(UIColor.white, for: UIControl.State.normal)
         self.button.backgroundColor = UIColor.black
+        
+        // Change buttons in scope
+        for button in buttonScope {
+            button.setTitleColor(UIColor.white, for: UIControl.State.normal)
+            button.backgroundColor = UIColor.red
+        }
     }
 
     @IBAction func clickButton(_ button: UIButton) {
+        
         // Change label
         self.label.text = "Hello, label!"
         self.label.font = label.font.withSize(35)
@@ -34,6 +42,10 @@ class ViewController: UIViewController {
         let uiViewTag: Int = button.tag
         if uiViewTag == 100 {
             print(uiViewTag)
+        } else if uiViewTag == 10 {
+            print("Click Button_1")
+        } else if uiViewTag == 20 {
+            print("Click Button_2")
         }
         
         // Show label
